02f0f5aa08c485e71e1f30892200ac6c48742132
Prakash Anandakumar <prakash.anandakumar@acentura.com>
2023-10-03 18:01:12 +0530

    Added all

---
+++ src/componets/Navbar.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
+class Navbar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cartCount: 0,
+    };
+  }
+
+  componentDidUpdate(state) {
+    const { cartReducer } = this.props;
+    if (cartReducer.cart !== state.cartReducer.cart)
+      this.setState({ cartCount: cartReducer.cart.length });
+  }
+  render() {
+    const { cartCount } = this.state;
+    return (
+      <>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+       
+          <div>
+            <Link to="/" className="navLink">
+              Home
+            </Link>
+            <Link to="/cart" className="navLink">
+              Cart
+            </Link>
+            <span className="cartCount"> Cart items : {cartCount}</span>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  cartReducer: state.cartReducer,
+});
+
+export default connect(mapStateToProps)(Navbar);
